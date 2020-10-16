1aed854e6d8eac65eb0bc6d9bcfc5ff03bf79422
ReganChon <349157189573>
2020-10-16 15:24:32 +0800

    组件live

--- export_mine/src/main/java/com/example/export_mine/MineUtils.kt
+++
@@ -1,8 +0,0 @@
-package com.example.export_mine
-
-import androidx.fragment.app.Fragment
-import com.alibaba.android.arouter.launcher.ARouter
-
-
-fun getMineFragment(): Fragment =
-    ARouter.getInstance().build("/mine/fragment").navigation() as Fragment
