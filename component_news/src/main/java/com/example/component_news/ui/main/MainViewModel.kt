db3e5559af88b7b6ab7aea57c0c4cdbf2b1e93bf
ReganChon <349157189573>
2020-10-16 15:57:02 +0800

    我的组件

--- component_news/src/main/java/com/example/component_news/ui/main/MainViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.component_news.ui.main
-
-import androidx.lifecycle.ViewModel
-
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
--- export_news/src/main/java/com/example/export_news/NewsTable.kt
+++
@@ -1,8 +0,0 @@
-package com.example.export_news
-
-import com.alibaba.android.arouter.launcher.ARouter
-
-fun getNewsService(): INewsService =
-    ARouter.getInstance().build("/component2/news/service").navigation() as INewsService
-
-fun getNewsList(): List<String> = getNewsService().getNewsList()
