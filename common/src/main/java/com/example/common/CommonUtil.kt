c764ab2ed6f3745b9d18c51360cc503e6d2ddfa2
ReganChon <349157189573>
2020-10-16 16:28:56 +0800

    common整合

---
+++ common/src/main/java/com/example/common/CommonUtil.kt
@@ -0,0 +1,14 @@
+package com.example.common
+
+import android.util.Log
+
+class CommonUtil {
+
+    companion object {
+
+        fun testPrint() {
+            Log.i("zujianhuademo", "打印测试")
+        }
+    }
+
+}
