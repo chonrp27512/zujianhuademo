041bbc2760eb9c1b9da29a58c1e72389f242a8ce
ReganChon <349157189573>
2020-10-19 10:23:23 +0800

    mine整合

--- component_mine/src/main/java/com/example/component_mine/MineActivity.kt
+++ component_mine/src/main/java/com/example/component_mine/MineActivity.kt
@@ -1,31 +1,31 @@
-//package com.example.component_mine
-//
-//import android.os.Bundle
-//import android.util.Log
-//import com.google.android.material.appbar.CollapsingToolbarLayout
-//import com.google.android.material.floatingactionbutton.FloatingActionButton
-//import com.google.android.material.snackbar.Snackbar
-//import androidx.appcompat.app.AppCompatActivity
-//import com.alibaba.android.arouter.facade.annotation.Route
-//import com.example.export_news.getNewsList
-//
-//@Route(path = "/component1/mine")
-//class MineActivity : AppCompatActivity() {
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_mine)
-//        setSupportActionBar(findViewById(R.id.toolbar))
-//        findViewById<CollapsingToolbarLayout>(R.id.toolbar_layout).title = title
-//        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                .setAction("Action", null).show()
-//        }
-//
-//        //获取其他组件的服务，并且获取其他组件的数据 ，也可以通过eventbus来通信
-//        val newsList = getNewsList()
-//        for (index in newsList) {
-//            Log.d("xxx", index)
-//        }
-//    }
-//}
+package com.example.component_mine
+
+import android.os.Bundle
+import android.util.Log
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.alibaba.android.arouter.facade.annotation.Route
+import com.example.export_news.getNewsList
+
+@Route(path = "/component1/mine")
+class MineActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mine)
+        setSupportActionBar(findViewById(R.id.toolbar))
+        findViewById<CollapsingToolbarLayout>(R.id.toolbar_layout).title = title
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
+        //获取其他组件的服务，并且获取其他组件的数据 ，也可以通过eventbus来通信
+        val newsList = getNewsList()
+        for (index in newsList) {
+            Log.d("xxx", index)
+        }
+    }
+}
