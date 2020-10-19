794b35694c3f981dbd09d58ba0e2433bb79b16d1
ReganChon <349157189573>
2020-10-19 10:46:32 +0800

    export_news 版本+1 path

--- export_news/src/main/java/com/example/export_news/NewsTable.kt
+++ export_news/src/main/java/com/example/export_news/NewsTable.kt
@@ -3,6 +3,6 @@ package com.example.export_news
 import com.alibaba.android.arouter.launcher.ARouter
 
 fun getNewsService(): INewsService =
-    ARouter.getInstance().build("/component2/news/service").navigation() as INewsService
+    ARouter.getInstance().build("/export/news/service").navigation() as INewsService
 
 fun getNewsList(): List<String> = getNewsService().getNewsList()
